c71a9b88bb39d6afc5d74ccf87a4a5d323d786c0
litu04 <debadattakar96@gmail.com>
2021-06-06 11:17:52 +0530

    router and controllers added

--- src/app.js
+++ src/app.js
@@ -10,10 +10,57 @@ app.set('views',path.join(__dirname,'views'));
 
 app.use(express.static('public'));
 
+// const accountData = fs.readFileSync("./json/accounts.json","utf-8");
+// //console.log(accountData);
+// const accounts = JSON.parse(accountData);
+// //console.log("account details",accounts);
+
+// const userData = fs.readFileSync('./json/users.json',"utf-8");
+// const users = JSON.parse(userData);
+// //console.log("user details",users);
+
+const accountData = fs.readFileSync(
+    path.join(__dirname, 'json', 'accounts.json'), 'utf8'
+);
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(
+    path.join(__dirname, 'json', 'users.json'), 'utf8'
+)
+const users = JSON.parse(userData);
+
 app.get('/',function(req,res){
 
     return res.render('index',{
-        title: 'Index'
+        title: 'Account Summary',
+        accounts: accounts
+    })
+})
+
+app.get('/savings',function(req,res){
+
+    return res.render('account',{
+        account: accounts.savings
+    })
+})
+
+app.get('/checking',function(req,res){
+
+    return res.render('account',{
+        account: accounts.checking
+    })
+})
+
+app.get('/credit',function(req,res){
+
+    return res.render('account',{
+        account: accounts.credit
+    })
+})
+
+app.get('/profile',function(req,res){
+    return res.render('profile',{
+        user:users[0]
     })
 })
 
